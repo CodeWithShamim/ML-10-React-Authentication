3345442e9b0b5b27a51544f2430d223d07fab730
codewithshamim <shamimislamonline@gmail.com>
2022-04-07 22:57:16 +0600

    update profile added

--- Module 57 ( Email Password Authentication And Log In Form)/email-pass-auth/src/components/LoginForm/LoginForm.js
+++ Module 57 ( Email Password Authentication And Log In Form)/email-pass-auth/src/components/LoginForm/LoginForm.js
@@ -4,17 +4,21 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  updateProfile,
 } from "firebase/auth";
 import app from "../../firebase.init";
 
 const auth = getAuth(app);
 
 const LoginForm = () => {
-  const [email, setEmail] = useState([]);
-  const [password, setPassword] = useState([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   // validation
   const [validated, setValidated] = useState(false);
   const [registered, setRegistered] = useState(false);
+  const [name, setName] = useState("");
 
   const handleEmailBlur = (event) => {
     setEmail(event.target.value);
@@ -48,12 +52,22 @@ const LoginForm = () => {
       createUserWithEmailAndPassword(auth, email, password)
         .then((result) => {
           console.log(result);
+          verifyEmail();
+          updateProfile(auth.currentUser, { displayName: name })
+            .then(() => {
+              console.log("profile updated!");
+            })
+            .catch((error) => console.log(error));
         })
         .catch((error) => {
           console.log(error);
           // ..
         });
+      // sendEmailVerification
     }
+
+    // setEmail([]);
+    // setPassword([]);
 
     e.preventDefault();
   };
@@ -62,10 +76,41 @@ const LoginForm = () => {
     setRegistered(e.target.checked);
   };
 
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser).then(() => {
+      console.log("Email verification sent!");
+    });
+  };
+
+  // handlePasswordReset
+  const handlePasswordReset = () => {
+    sendPasswordResetEmail(auth, email)
+      .then(() => console.log("Password reset send!"))
+      .catch((error) => console.log(error));
+  };
+
+  const handleNameBlur = (e) => {
+    setName(e.target.value);
+  };
   return (
     <div className="w-50 mx-auto">
       <h2>Please {registered ? "Login" : "Registered"}</h2>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        {!registered && (
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name:</Form.Label>
+            <Form.Control
+              onBlur={handleNameBlur}
+              type="text"
+              placeholder="Enter name"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter your name.
+            </Form.Control.Feedback>
+          </Form.Group>
+        )}
+
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
@@ -98,6 +143,11 @@ const LoginForm = () => {
             label="Already registered?"
           />
         </Form.Group>
+        <Button onClick={handlePasswordReset} variant="link">
+          Forgoted pasword?
+        </Button>
+        <br />
+        <br />
         <Button variant="primary" type="submit">
           {registered ? "Login" : "Register"}
         </Button>
