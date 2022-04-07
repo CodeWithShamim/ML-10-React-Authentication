e87485c717d27b7adb85bdf4105712e983ae7940
codewithshamim <shamimislamonline@gmail.com>
2022-04-07 17:20:51 +0600

    form validatin added

--- Module 57 ( Email Password Authentication And Log In Form)/email-pass-auth/src/App.js
+++ Module 57 ( Email Password Authentication And Log In Form)/email-pass-auth/src/App.js
@@ -1,44 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import LoginForm from "./components/LoginForm/LoginForm";
 // import { getAuth } from "/firebase/auth";
 // import app from "./firebase.init";
 
 // const auth = getAuth(app);
 function App() {
-  const handleEmailBlur = (event) => {
-    console.log(event.target.value);
-  };
-
-  const handlePasswordBlur = (event) => {
-    console.log(event.target.value);
-  };
-
-  const handleFormSubmit = (e) => {
-    console.log("Form submitted!!");
-    e.preventDefault();
-  };
-
   return (
     <div className="App">
-      <form onSubmit={handleFormSubmit}>
-        <input
-          onBlur={handleEmailBlur}
-          type="email"
-          name=""
-          id=""
-          placeholder="email here"
-        />
-        <br />
-        <input
-          onBlur={handlePasswordBlur}
-          type="password"
-          name=""
-          id=""
-          placeholder="password here"
-        />
-        <br />
-        <input type="submit" value="Login" />
-      </form>
+      <LoginForm></LoginForm>
     </div>
   );
 }
---
+++ Module 57 ( Email Password Authentication And Log In Form)/email-pass-auth/src/components/LoginForm/LoginForm.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from "../../firebase.init";
+
+const auth = getAuth(app);
+
+const LoginForm = () => {
+  const [email, setEmail] = useState([]);
+  const [password, setPassword] = useState([]);
+  // validation
+  const [validated, setValidated] = useState(false);
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    // validation start
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+    setValidated(true);
+    // validation start
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+        // ..
+      });
+
+    e.preventDefault();
+  };
+
+  return (
+    <div className="w-50 mx-auto">
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            onBlur={handleEmailBlur}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please choose valid email.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            onBlur={handlePasswordBlur}
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please choose valid password.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default LoginForm;
