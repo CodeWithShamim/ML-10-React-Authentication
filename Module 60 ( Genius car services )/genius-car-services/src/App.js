d732335c51061d970b4ea3f0d18d272d816090bc
codewithshamim <shamimislamonline@gmail.com>
2022-04-14 19:24:30 +0600

    firebase i & component updated

--- Module 60 ( Genius car services )/genius-car-services/src/App.js
+++ Module 60 ( Genius car services )/genius-car-services/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Header from "./Pages/Shared/Header";
-import Home from "./Pages/Home/Home";
+import Home from "./Pages/Home/Home/Home";
+import Footer from "./Pages/Shared/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
